80b6e18f13603a1678c68a9596e7ab1543ca73b7
hgvxxi7 <hgvxxi7@gmail.com>
2018-08-30 20:49:39 +0300

    initial commit

---
+++ HelloWorld1WEEK/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1WEEK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salutation = "Hello World !!!"; // объявление переменной salutation типа string
+                                                   // и присвоение переменной какого-то значения, например "Hello World"
+
+            System.Console.WriteLine(salutation); // команда для вывода значения переменной salutation на екран вконсоли
+            Console.ReadKey(); // команда остановки консоли (пока не будет нажата любая кнопка)
+        }
+    }
+}
